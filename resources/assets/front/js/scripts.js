6044e4ebd390daeb7abedcfdbc711e06761ddc1b
Coca95 <coralie.delmee@hotmail.be>
2022-08-01 13:24:35 -0400

    Over

--- resources/assets/front/js/scripts.js
+++ resources/assets/front/js/scripts.js
@@ -2,5 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
     require("./modules/_navbar.js");
     require("./modules/_slider.js");
     require("./modules/_darktheme.js");
+    require("./modules/_contact");
 })
 
